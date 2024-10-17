a58709d694da34bd7922fa552f951693674b2b43
tarapash <153995889+tarapash@users.noreply.github.com>
2024-10-17 12:46:02 +0300

    Add files via upload

---
+++ NewsPortal/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+   path('admin/', admin.site.urls),
+   path('accounts/', include('allauth.urls')),
+   path('', include('Portal_app.urls')),
+]
+
+   
+
---
+++ Portal_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PortalAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Portal_app'
+
